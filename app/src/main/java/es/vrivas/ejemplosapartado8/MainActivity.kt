bd68020edac4ef6362aacc2ecf6faa41de278bca
Victor Rivas <vrivas@ujaen.es>
2020-11-01 17:39:59 +0100

    La propia actividad implementa la interfaz View.OnClickListener

--- app/src/main/java/es/vrivas/ejemplosapartado8/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado8/MainActivity.kt
@@ -1,47 +1,58 @@
 package es.vrivas.ejemplosapartado8
 
+import android.annotation.SuppressLint
 import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
+import android.view.View.generateViewId
 import android.widget.Button
 import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     var viendo_imagen: Int =0
+    lateinit var bt_reiniciar: Button
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Modifico las propiedades de los botones existentes
-        bt_poner.setText( "PONER IMAGEN")
-        bt_quitar.text="QUITAR IMAGEN"
+        bt_poner.setText( getString(R.string.poner_imagen))
+        bt_quitar.text=getString(R.string.quitar_imagen)
 
         // Creo un nuevo botón programáticamente
-        var bt_reiniciar = Button(this)
         val params = FrameLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT )
-
-        params.gravity=Gravity.BOTTOM+ Gravity.LEFT
+        bt_reiniciar= Button(this)
+        params.gravity=Gravity.BOTTOM+ Gravity.START
         params.bottomMargin=20
         params.leftMargin=350
 
-        bt_reiniciar.text="REINICIAR IMAGEN"
+        bt_reiniciar.text=getString(R.string.reiniciar_imagen)
+        bt_reiniciar.id= 1_001 // LE asigno un id cualquiera
+        Log.d("VICTOR", "el id es "+bt_reiniciar.id)
 
         addContentView(bt_reiniciar, params)
+
+        bt_poner.setOnClickListener(this)
+        bt_quitar.setOnClickListener(this)
+        bt_reiniciar.setOnClickListener(this)
     }
 
     // Opción 1: Usamos un callback para cada botón
     @Suppress( "UNUSED_PARAMETER")
-    fun bt_poner_click(view: View){
+    fun bt_poner_click(view: View?){
         viendo_imagen+=if (viendo_imagen<2) 1 else 0
         im_gafas.visibility=if (viendo_imagen>0) View.VISIBLE else View.INVISIBLE
         im_barba.visibility=if (viendo_imagen>1) View.VISIBLE else View.INVISIBLE
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun bt_quitar_click(view: View){
+    fun bt_quitar_click(view: View?){
         viendo_imagen-=if (viendo_imagen>0) 1 else 0
         im_gafas.visibility=if (viendo_imagen>0) View.VISIBLE else View.INVISIBLE
         im_barba.visibility=if (viendo_imagen>1) View.VISIBLE else View.INVISIBLE
@@ -49,14 +60,25 @@ class MainActivity : AppCompatActivity() {
 
 
     // Opción 2: Usamos un solo callback para todos los botones, identificando cuál ha sido pulsado
-    @Suppress( "UNUSED_PARAMETER")
-    fun bt_quitar_poner_click(view: View){
-        when( view.id ) {
+    fun bt_quitar_poner_click(view: View?){
+        when( view?.id ) {
             R.id.bt_quitar -> viendo_imagen-=if (viendo_imagen>0) 1 else 0
             R.id.bt_poner -> viendo_imagen+=if (viendo_imagen<2) 1 else 0
         }
         im_gafas.visibility=if (viendo_imagen>0) View.VISIBLE else View.INVISIBLE
         im_barba.visibility=if (viendo_imagen>1) View.VISIBLE else View.INVISIBLE
     }
+
+    // Opción 3: La actividad implementa los métodos de la interfaz View.OnClickListener
+    override fun onClick(view: View?) {
+        when( view?.id ) {
+            bt_quitar.id -> viendo_imagen-=if (viendo_imagen>0) 1 else 0
+            bt_poner.id -> viendo_imagen+=if (viendo_imagen<2) 1 else 0
+            bt_reiniciar.id -> viendo_imagen=0
+        }
+        im_gafas.visibility=if (viendo_imagen>0) View.VISIBLE else View.INVISIBLE
+        im_barba.visibility=if (viendo_imagen>1) View.VISIBLE else View.INVISIBLE
+    }
+
 
 }
